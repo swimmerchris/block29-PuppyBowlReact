5fcd3475d719e23c837b5d20cf0128377f7c1b38
Christopher Bryan <swimmer.chris@gmail.com>
2024-01-21 23:25:31 -0500

    current working solution with some additional css, more to come.

--- src/api/puppyBowlApi.js
+++ src/api/puppyBowlApi.js
@@ -1,20 +1,13 @@
-// Import createApi (function to create an API service) and fetchBaseQuery (a basic fetch wrapper)
-// from Redux Toolkit Query's React-specific entry point
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// Define an API using createApi
 export const puppyBowlApi = createApi({
-  // Unique string used in constructing Redux action types, state selectors, and React hook names
   reducerPath: "puppyBowlApi",
-  // Define a base query function that all endpoints will use as the base of their request
   baseQuery: fetchBaseQuery({
-    // The base URL for all requests
     baseUrl: "https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-pt-web-b/"
   }),
-  // Define endpoints for our API service
+
   tagTypes: ["Post"],
   endpoints: (builder) => ({
-    // Define an endpoint that fetches players
     fetchPlayers: builder.query({
       query: () => "/players",
       providesTags: ["Post"]
